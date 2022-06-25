f226fd79f57483f4db95f83edb114f7daa85341e
Shun Ueda <hasundue@gmail.com>
2022-06-25 11:15:10 +0900

    test: add tests for durable object

--- test.ts
+++ test.ts
@@ -1,23 +1,27 @@
 import { readLines } from "https://deno.land/std@0.144.0/io/mod.ts";
 import { assertObjectMatch } from "https://deno.land/std@0.144.0/testing/asserts.ts";
 
-Deno.test("router", async () => {
+async function startServer(file: string, ...options: string[]) {
   const proc = Deno.run({
-    cmd: ["denoflare", "serve", "./examples/router.ts"],
+    cmd: ["denoflare", "serve", file].concat(options),
     stdout: "piped",
   });
 
   for await (const line of readLines(proc.stdout)) {
-    const matched = line.match(/http:\/\/localhost:\d+/);
-    if (matched) break;
+    if (line.match("Local server running")) break;
   }
 
-  const host = "http://localhost:8080";
+  return proc;
+}
 
-  const knock = async (path: string, method?: string) => {
-    const response = await fetch(host + path, { method: method ?? "GET" });
-    return await response.json();
-  };
+const knock = async (path: string, method?: string) => {
+  const host = "http://localhost:8080";
+  const response = await fetch(host + path, { method: method ?? "GET" });
+  return await response.json();
+};
+
+Deno.test("router", async () => {
+  const proc = await startServer("./examples/router.ts");
 
   assertObjectMatch(
     await knock("/"),
@@ -37,6 +41,22 @@ Deno.test("router", async () => {
   assertObjectMatch(
     await knock("/object/test"),
     { name: "test" },
+  );
+
+  proc.stdout.close();
+  proc.close();
+});
+
+Deno.test("durable object", async () => {
+  const proc = await startServer(
+    "./examples/durable_object.ts",
+    "--do-namespace-binding",
+    "do:local:DO",
+  );
+
+  assertObjectMatch(
+    await knock("/test"),
+    { id: "8feb29077a1df95bd8e261f2a94a8fe5ccb19ba61c4c0873d391e987982fbbd3" },
   );
 
   proc.stdout.close();

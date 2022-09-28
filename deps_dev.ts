a0355c4d1407e3fa30ec8115b8eace8cb3a2ad0d
GitHub <noreply@github.com>
2022-09-28 23:32:16 +0000

    build(deps-dev): update dev dependencies

--- deps_dev.ts
+++ deps_dev.ts
@@ -1,5 +1,5 @@
-export { readLines } from "https://deno.land/std@0.157.0/io/mod.ts";
+export { readLines } from "https://deno.land/std@0.158.0/io/mod.ts";
 export {
   assertEquals,
   assertObjectMatch,
-} from "https://deno.land/std@0.157.0/testing/asserts.ts";
+} from "https://deno.land/std@0.158.0/testing/asserts.ts";

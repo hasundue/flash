e55475e5e7fcc1a575b7f22abcb879f44c9c2d5c
hasundue <hasundue@gmail.com>
2022-12-22 14:39:30 +0900

    snapshot

--- examples/hello_world.ts
+++ examples/hello_world.ts
@@ -1,3 +1,66 @@
-import { flare } from "../mod.ts";
+import { flash, Resource } from "../cloudflare.ts";
 
-export default flare({ "/": "Hello, World!" });
+type Repository = {
+  owner: string;
+  repo: string;
+};
+
+const repos: Resource<Repository> = ({ storage }) => ({
+  GET: async () => {
+    return await storage.list();
+  },
+  "/:owner/:repo": (params: { owner: string; repo: string }) => ({
+    GET: async () => {
+      return await storage.get(`${params.owner}/${params.repo}`);
+    },
+    "issues": issues,
+  }),
+});
+
+type Issue = {
+  id: number;
+  creator: string;
+  date: Date;
+};
+
+const issues: Resource<Issue> = ({ storage }) => ({
+  /**
+   * List issues
+   */
+  GET: async (params: {
+    creator?: string;
+    since?: Date;
+    until?: Date;
+  }) => {
+    return (await storage.list()).filter((it) =>
+      it.creator === (params.creator ?? it.creator) &&
+      it.date >= (params.since ?? 0) &&
+      it.date <= (params.until ?? Infinity)
+    );
+  },
+
+  /**
+   * Create an issue
+   */
+  POST: async (body: {
+    creator: string;
+  }) => {
+    const id = await storage.count();
+    await storage.put(id, {
+      id,
+      creator: body.creator,
+      date: new Date(),
+    });
+  },
+
+  /**
+   * Get an issue specified by ID
+   */
+  "/:id": async (params: { id: string }) => {
+    return await storage.get(params.id);
+  },
+});
+
+export const app = flash.rest({
+  "/repos": repos,
+});

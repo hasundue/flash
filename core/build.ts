5c78bba0be7abf788b5302cf5fef21c81de6d3c1
denopendabot <denopendabot@github.com>
2023-01-06 02:20:39 +0000

    build(deps): bump std from 0.170.0 to 0.171.0

--- core/build.ts
+++ core/build.ts
@@ -1,5 +1,5 @@
-import { mapEntries } from "https://deno.land/std@0.170.0/collections/map_entries.ts";
-import { dirname, join } from "https://deno.land/std@0.170.0/path/mod.ts";
+import { mapEntries } from "https://deno.land/std@0.171.0/collections/map_entries.ts";
+import { dirname, join } from "https://deno.land/std@0.171.0/path/mod.ts";
 import { Hono } from "https://deno.land/x/hono@v2.1.4/mod.ts";
 import { getTypeScriptReader } from "https://esm.sh/typeconv@1.8.0";
 import { Application } from "./application.ts";

c824035ff5c5c2b701b0aa19fbb2a33ff26071c4
denopendabot <denopendabot@github.com>
2023-01-09 00:07:21 +0000

    build(deps): bump hono from v2.7.1 to v2.7.2

--- core/build.ts
+++ core/build.ts
@@ -1,6 +1,6 @@
 import { mapEntries } from "https://deno.land/std@0.171.0/collections/map_entries.ts";
 import { dirname, join } from "https://deno.land/std@0.171.0/path/mod.ts";
-import { Hono } from "https://deno.land/x/hono@v2.7.1/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v2.7.2/mod.ts";
 import { getTypeScriptReader } from "https://esm.sh/typeconv@1.8.0";
 import { Application } from "./application.ts";
 import { AbstractResourceType, Resource } from "./resource.ts";

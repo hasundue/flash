17ccda2e009aab79c130b87460cee96250e8b2d6
denopendabot <denopendabot@github.com>
2023-03-10 00:08:04 +0000

    build(deps): bump std from 0.171.0 to 0.178.0

--- core/build.ts
+++ core/build.ts
@@ -1,5 +1,5 @@
-import { mapEntries } from "https://deno.land/std@0.171.0/collections/map_entries.ts";
-import { dirname, join } from "https://deno.land/std@0.171.0/path/mod.ts";
+import { mapEntries } from "https://deno.land/std@0.178.0/collections/map_entries.ts";
+import { dirname, join } from "https://deno.land/std@0.178.0/path/mod.ts";
 import { Hono } from "https://deno.land/x/hono@v2.7.2/mod.ts";
 import { getTypeScriptReader } from "https://esm.sh/typeconv@1.8.0";
 import { Application } from "./application.ts";
--- platforms/upstash.ts
+++ platforms/upstash.ts
@@ -1,7 +1,7 @@
-import { filterValues } from "https://deno.land/std@0.171.0/collections/filter_values.ts";
-import { intersect } from "https://deno.land/std@0.171.0/collections/intersect.ts";
-import { union } from "https://deno.land/std@0.171.0/collections/union.ts";
-import { errors } from "https://deno.land/std@0.171.0/http/http_errors.ts";
+import { filterValues } from "https://deno.land/std@0.178.0/collections/filter_values.ts";
+import { intersect } from "https://deno.land/std@0.178.0/collections/intersect.ts";
+import { union } from "https://deno.land/std@0.178.0/collections/union.ts";
+import { errors } from "https://deno.land/std@0.178.0/http/http_errors.ts";
 import {
   Redis as RedisClient,
   RedisConfigDeno,

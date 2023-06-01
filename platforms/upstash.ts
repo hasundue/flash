166d429fd475340c0b76e38c8de6a22f1ccdfa38
denopendabot <denopendabot@github.com>
2023-06-01 00:08:53 +0000

    build(deps): bump upstash_redis from v1.20.6 to v1.21.0

--- platforms/upstash.ts
+++ platforms/upstash.ts
@@ -5,7 +5,7 @@ import { errors } from "https://deno.land/std@0.190.0/http/http_errors.ts";
 import {
   Redis as RedisClient,
   RedisConfigDeno,
-} from "https://deno.land/x/upstash_redis@v1.20.6/mod.ts";
+} from "https://deno.land/x/upstash_redis@v1.21.0/mod.ts";
 import { AbstractResourceType, ResourceObject } from "../core/resource.ts";
 import {
   ConcreteQueryOperatorRecord,

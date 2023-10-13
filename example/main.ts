bac48287e12738aef503c9f3416668b32afe9559
denopendabot <denopendabot@github.com>
2023-10-13 00:07:32 +0000

    build(deps): bump std from 0.203.0 to 0.204.0

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.204.0/http/server.ts";
 import { build } from "../mod.ts";
 
 const app = await build("./app.ts");
